6f01e4170a80eb2b28a036e1d8c1dd3817361b7b
Alex Gergel <alexgergel@gmail.com>
2022-01-12 20:48:18 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 def create_dict(*args):
-    # write your code here
+    dicto = {}
+    for index, arg in enumerate(args):
+        if isinstance(arg, tuple):
+            for arg1 in arg:
+                if isinstance(arg1, (list, dict, set)):
+                    print(f"Cannot add {arg1} to the dict!")
+                    break
+                else:
+                    dicto[arg] = index
+        elif isinstance(arg, (list, dict, set)):
+            print(f"Cannot add {arg} to the dict!")
+        else:
+            dicto[arg] = index
+    return dicto
+
     pass
