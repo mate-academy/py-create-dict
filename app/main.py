6f2c021e672fdc1980606feb15be892272851a17
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-01-12 16:24:00 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ def create_dict(*args):
     list_arg = [arg for arg in args]
     result_dict = {}
     for n, element in enumerate(list_arg):
-        if isinstance(element, (str, float, int, tuple))\
-                or isinstance(element, types.FunctionType):
+        if isinstance(element, (str, float, int, tuple, bool)) \
+                or isinstance(element, types.FunctionType) or element is None:
             result_dict[element] = n
         else:
             print(f"Cannot add {element} to the dict!")
