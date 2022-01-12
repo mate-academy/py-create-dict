c6a1ac9a0b0102d2a1c994e4992fc576617a91b7
Kalyta Valerii <deptovka@gmail.com>
2022-01-12 17:09:05 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
 def create_dict(*args):
-    # write your code here
-    pass
+    result = {}
+
+    for x, y in enumerate(args):
+        if isinstance(y, (list, dict, set)):
+            print(f"Cannot add {y} to the dict!")
+            continue
+        result[y] = x
+
+    return result
