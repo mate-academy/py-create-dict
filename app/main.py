895d2669c91aa07309e4691560016a3879922df1
Dmitry Ivanov <karward94@gmail.com>
2022-01-12 16:18:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
 def create_dict(*args):
-    # write your code here
-    pass
+    result_dict = {}
+    for n, arg in enumerate(args):
+        if not isinstance(arg, list | set | dict):
+            try:
+                result_dict[arg] = n
+            except TypeError:
+                print(f"Cannot add {arg} to the dict!")
+        else:
+            print(f"Cannot add {arg} to the dict!")
+    return result_dict
