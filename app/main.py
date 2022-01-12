933a87b98d77dcdfcaca343d7fefc5095e765142
Zikkolion <nmadaraosnova835@gmail.com>
2022-01-12 23:17:07 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
 def create_dict(*args):
     # write your code here
-    pass
+    dict2 = {}
+
+    for i, element in enumerate(args):
+        if isinstance(element, (list, dict, set)):
+            print(f"Cannot add {element} to the dict!")
+        elif isinstance(element, tuple):
+            for j in element:
+                if isinstance(j, (list, dict, set)):
+                    print(f"Cannot add {element} to the dict!")
+                    break
+            else:
+                dict2[element] = i
+        else:
+            dict2[element] = i
+
+    return dict2
