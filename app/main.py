de8e34cdbc347c4b871288264e3d94130a746225
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-01-12 18:48:47 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,20 +1,16 @@
 def create_dict(*args):
     param = list(args)
     resultd = {}
-    count = 0
     for i in range(len(param)):
-        if isinstance(param[i], list) or isinstance(param[i], set) \
-                or isinstance(param[i], dict):
+        if isinstance(param[i], (list, set, dict)):
             print(f"Cannot add {param[i]} to the dict!")
         if param[i] is None or callable(param[i]) \
-                or isinstance(param[i], int) \
-                or isinstance(param[i], float) or isinstance(param[i], str) \
-                or isinstance(param[i], bool):
+                or isinstance(param[i], (int, float, str, bool)):
             resultd[param[i]] = i
         if isinstance(param[i], tuple):
+            count = 0
             for j in param[i]:
-                if isinstance(j, list) or isinstance(j, set) \
-                        or isinstance(j, dict):
+                if isinstance(j, (list, set, dict)):
                     count += 1
             if count == 0:
                 resultd[param[i]] = i
