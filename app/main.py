be4694c5de0f021df2c854bf8a0098052e6181c4
Vlad Tsvigun <vl.tsv@outlook.com>
2022-01-12 17:19:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ import types
 def create_dict(*args):
     dictionary = {}
     for argument in args:
-        if not isinstance(argument, (int, float, str, tuple, types.FunctionType)):
+        if not isinstance(argument,
+                          (int, float, str, tuple, types.FunctionType)):
             print(f"Cannot add {argument} to the dict!")
         else:
             dictionary[argument] = args.index(argument)
