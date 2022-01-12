33ee202b46b9fc79286b6c5f1dc588efb37e6933
Alexandr-Semenishin <97320430+Alexandr-Semenishin@users.noreply.github.com>
2022-01-12 23:39:34 +0200

    half-solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 def create_dict(*args):
-    # write your code here
-    pass
+    out = {}
+    for index, key in enumerate(args):
+        # if callable(key): ?
+        #     continue      ?
+        if isinstance(key, (int, float, str, bool)):
+            out[key] = index
+            continue
+        if isinstance(key, tuple):
+            if not any([isinstance(key0, (set, list)) for key0 in key]):
+                out[key] = index
+                continue
+        print(f"Cannot add {key} to the dict!")
+    return out
