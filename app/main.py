c715f48753c70d146113242901e4bf8effdf87b4
Ihor Olifirenko <igoroliphirenko@gmail.com>
2022-01-12 17:46:10 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 def create_dict(*args):
-    # write your code here
-    pass
+    result_dict = {}
+    for i in range(len(args)):
+        if isinstance(args[i], (list, set, dict)) or args[i] is None:
+            print(f"Cannot add {args[i]} to the dict!")
+            continue
+        else:
+            result_dict.update({args[i]: i})
+    return result_dict
