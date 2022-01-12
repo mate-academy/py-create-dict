d53355f822cf8ca98f933d7b697241e788becbbb
Alex Zhidkov <sashazudkov.@gmail.com>
2022-01-12 17:13:47 +0200

    Solution + Optional Task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
 def create_dict(*args):
-    # write your code here
-    pass
+    res_dict = {}
+    for counter, i in enumerate(args):
+        if isinstance(i, (int, str, float, bool)) or callable(i) or i is None or check(i):
+            res_dict.update({i: counter})
+        else:
+            print(f"Cannot add {i} to the dict!")
+    return res_dict
+
+
+def check(param):
+    if isinstance(param, (int, str, float, bool)) or callable(param) or param is None:
+        return True
+    if isinstance(param, tuple):
+        return all(check(j) for j in param)
+    return False
