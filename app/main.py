40c2f9eed47d34cc101363838fb2d058da0b2438
NikitaZH1812 <zhukovna1812@gmail.com>
2022-01-13 00:21:18 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
 def create_dict(*args):
-    # write your code here
-    pass
+    result = {}
+    for i, argument in enumerate(args):
+        if isinstance(argument, (list, dict, set) 
+                        or argument is None or type(argument) == 'function'):
+            print(f"Cannot add {argument} to the dict!")
+        else:
+            result[argument] = i
+
+    return result
