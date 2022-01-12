74aae3acddc9dbcbf976e9b3c6a404b5d133c555
alix makarova <alix.makarova@gmail.com>
2022-01-12 17:22:50 +0200

    Solution with two loops :(

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
 def create_dict(*args):
-    # write your code here
-    pass
+
+    result = {}
+
+    for i in range(len(args)):
+        if not isinstance(args[i], (list, dict, set)):
+            if isinstance(args[i], tuple):
+                count = 0
+                for item in args[i]:
+                    if isinstance(item, (list, dict, set)):
+                        print(f"Cannot add {args[i]} to the dict!")
+                        count = 1
+                        break
+                if count == 0:
+                    result[args[i]] = i
+            else:
+                result[args[i]] = i
+
+        else:
+            print(f"Cannot add {args[i]} to the dict!")
+
+    return result
