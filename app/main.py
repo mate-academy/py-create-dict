98db725e65c3f8d381d4677d3bfe85a7c1bed85d
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-01-12 15:33:39 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
+import types
+
+
 def create_dict(*args):
-    # write your code here
-    pass
+    list_arg = [arg for arg in args]
+    result_dict = {}
+    for n, element in enumerate(list_arg):
+        if isinstance(element, (str, float, int, tuple))\
+                or isinstance(element, types.FunctionType):
+            result_dict[element] = n
+        else:
+            print(f"Cannot add {element} to the dict!")
+
+    return result_dict
