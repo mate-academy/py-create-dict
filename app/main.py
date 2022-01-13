d77cd16e9de077457348dbd905d80a9567acbb97
Dmitry Ivanov <karward94@gmail.com>
2022-01-13 18:11:11 +0200

    Fix solution2

--- app/main.py
+++ app/main.py
@@ -1,20 +1,18 @@
 def create_dict(*args):
     result_dict = {}
     for n, arg in enumerate(args):
-        if isinstance(arg, tuple):
-            temp_list = []
-            for arg2 in arg:
-                if not isinstance(arg2, (list, set, dict)):
-                    temp_list.append(arg2)
-                    continue
-                else:
-                    print(f"Cannot add {arg} to the dict!")
-                    temp_list = []
-            if temp_list:
-                tuple_temp = tuple(temp_list)
-                result_dict[tuple_temp] = n
-        elif not isinstance(arg, (list, set, dict)):
-            result_dict[arg] = n
-        else:
+        if isinstance(arg, (list, dict, set)):
             print(f"Cannot add {arg} to the dict!")
+        elif isinstance(arg, tuple):
+            h = True
+            for arg2 in arg:
+                if isinstance(arg2, (list, dict, set)):
+                    h = False
+                    print(f"Cannot add {arg} to the dict!")
+                    break
+                if h:
+                    result_dict[arg] = n
+        else:
+            result_dict[arg] = n
+
     return result_dict
