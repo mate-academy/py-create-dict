ad7d0efb64590829c94797aeb8f9dfc1e76151fa
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-01-13 13:44:38 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
 def create_dict(*args):
-    # write your code here
-    pass
+    result = {}
+    for index, arg in enumerate(args):
+        if isinstance(arg, (list, set, dict)):
+            print(f"Cannot add {arg} to the dict!")
+        elif isinstance(arg, tuple):
+            is_hashable = True
+
+            for item in arg:
+                if isinstance(item, (list, set, dict)):
+                    is_hashable = False
+                    print(f"Cannot add {item} to the dict!")
+                    break
+
+            if is_hashable:
+                result[arg] = index
+
+        else:
+            result[arg] = index
+
+    return result
