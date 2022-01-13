c1c0b6f4cb971f18a70c79b74819fb5345db959c
Rybak Dmytro <codingclaud@gmail.com>
2022-01-13 12:18:13 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,20 +5,14 @@ def create_dict(*args):
     for iterator, value in enumerate(args):
 
         if (
-            isinstance(value, list) or
-            isinstance(value, set) or
-            isinstance(value, dict)
+            isinstance(value, (list, set, dict))
         ):
             print(f"Cannot add {value} to the dict!")
             continue
         if isinstance(value, tuple):
             for j in value:
                 if (
-                    isinstance(j, list) or
-                    isinstance(j, set) or
-                    isinstance(j, dict) or
-                    j is None or
-                    callable(j)
+                    isinstance(j, (list, dict))
                 ):
                     print(f"Cannot add {value} to the dict!")
                     is_not_tuple = False
