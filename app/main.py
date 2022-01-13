546074e83984a0811fe32e7c342616ed56e080f8
Alexandr-Semenishin <97320430+Alexandr-Semenishin@users.noreply.github.com>
2022-01-13 16:45:33 +0200

    Merge branch 'develop' of github.com:Alexandr-Semenishin/py-create-dict into develop
    # Conflicts:
    #	app/main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 def create_dict(*args):
     out = {}
     for index, key in enumerate(args):
-        # if callable(key): ?
-        #     continue      ?
-        if isinstance(key, (int, float, str, bool)):
+        if isinstance(key, (int, float, str, bool)) or callable(key) or None:
             out[key] = index
             continue
         if isinstance(key, tuple):
-            if not any([isinstance(key0, (set, list)) for key0 in key]):
+            if not any([isinstance(key_key, (set, list)) for key_key in key]):
                 out[key] = index
                 continue
         print(f"Cannot add {key} to the dict!")
     return out
+
+    # Is it possible to leave such a construction, without (if elif else)?
+    # it seems to me that in this case it increases the size of the code
