b9a9b6b89315154bc62cfaad5d42d5372d074aeb
Alex Gergel <alexgergel@gmail.com>
2022-02-15 16:06:28 +0200

    Took me some time, but now the tests pass

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 def create_dict(*args):
     dicto = {}
     for index, arg in enumerate(args):
-        if isinstance(arg, (set, list, dict)):
-            print(f"Cannot add {arg} to the dict!")
-        elif isinstance(arg, tuple):
-            for i in arg:
-                if isinstance(i, (set, list, dict)):
-                    print(f"Cannot add {arg} to the dict!")
+        if isinstance(arg, tuple):
+            for element in arg:
+                if isinstance(element, (set, list, dict)):
+                    print(f"Cannot add {element} to the dict!")
                     break
                 else:
-                    dicto[arg] = i
+                    dicto[arg] = element
+        if isinstance(arg, (set, list, dict, bool)):
+            print(f"Cannot add {arg} to the dict!")
+
         elif not isinstance(arg, (set, list, dict)):
             dicto[arg] = index
 
